8d0070d1a2f5ec777e3865f96590b4b8d78d63d0
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2024-11-10 16:06:31 -0300

    Update AB#60 Atualizando tela de comandas

--- src/components/comandasBody.tsx
+++ src/components/comandasBody.tsx
@@ -3,21 +3,22 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 import { SimpleGrid } from '@chakra-ui/react'
 
 export default function ComandasBody(){
+    const isHeader = (index : integer) => {
+        return index == 0 ?  "Comandas" : "Comanda " + index;
+    }
     return (
     <> 
         <Flex style={containerStyle}>
             <Flex style={listStyle}>
-                <SimpleGrid minChildWidth={'30%'} gap={2}>
+                <SimpleGrid minChildWidth={'90%'}  gap={2}>
                     {[...Array(18)].map((_, index) => (
                         <Flex key={index} style={comandaStyle}>
-                            <Text style={comandaTextStyle}>{index + 1}</Text>
+                            <Text style={comandaTextStyle}>{isHeader(index)}</Text>
                         </Flex>
                     ))}
                 </SimpleGrid>
             </Flex>
-            <Flex style={comandaItensStyle}>
-
-            </Flex>
+            <Flex style={comandaItensStyle}></Flex>
         </Flex>
     </>
     )
--- src/utils/styles/comandasBodyStyle.ts
+++ src/utils/styles/comandasBodyStyle.ts
@@ -10,25 +10,28 @@ export const containerStyle: CSSProperties = {
 export const listStyle: CSSProperties = {
     height: '90%',
     borderColor: '#2a2a2a',
-    borderWidth: '4px',
+    borderWidth: '5px',
     width: '25%',
     flexDirection: 'column',
     overflowY: 'scroll',
-    padding: 6
+    padding: 6,
+    borderRadius: 5
 }
 
 export const comandaItensStyle: CSSProperties = {
     height: '90%',
     width: '70%',
     borderColor: '#2a2a2a',
-    borderWidth: '4px',
+    borderWidth: '5px',
+    borderRadius: 5
 }
 
 export const comandaStyle: CSSProperties = {
     backgroundColor: '#2a2a2a',
-    aspectRatio: 1,
+    aspectRatio: 5,
     justifyContent: 'center',
     alignItems: 'center',
+    borderRadius: 5
 }
 
 export const comandaTextStyle: CSSProperties = {
